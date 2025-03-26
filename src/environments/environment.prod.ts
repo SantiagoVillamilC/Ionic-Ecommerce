adc900467351266df9d4968e259079ceb248122b
SantiagoVillamilC <santyago4993@gmail.com>
2025-03-25 19:52:43 -0500

    integracion firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebaseConfig: {
+    apiKey: "AIzaSyAaG8tKUuhmH1nyPB8M94_zYx3_cdPWQZ0",
+    authDomain: "product-admin-app-ba18e.firebaseapp.com",
+    projectId: "product-admin-app-ba18e",
+    storageBucket: "product-admin-app-ba18e.firebasestorage.app",
+    messagingSenderId: "375266690139",
+    appId: "1:375266690139:web:9ae603df103772dda6eb7e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+
+  // Your web app's Firebase configuration
+  firebaseConfig: {
+    apiKey: "AIzaSyAaG8tKUuhmH1nyPB8M94_zYx3_cdPWQZ0",
+    authDomain: "product-admin-app-ba18e.firebaseapp.com",
+    projectId: "product-admin-app-ba18e",
+    storageBucket: "product-admin-app-ba18e.firebasestorage.app",
+    messagingSenderId: "375266690139",
+    appId: "1:375266690139:web:9ae603df103772dda6eb7e"
+  }
 };
 
 /*
