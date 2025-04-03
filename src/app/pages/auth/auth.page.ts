6e9c47ae35c38cf37b78f88009fbacfc9db85cc6
SantiagoVillamilC <santyago4993@gmail.com>
2025-04-02 20:57:04 -0500

    user methods

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from 'src/app/shared/components/header/header.compone
 import { LogoComponent } from 'src/app/shared/components/logo/logo.component';
 import { CustomInputComponent } from 'src/app/shared/components/custom-input/custom-input.component';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { User } from 'src/app/models/user.model';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-auth',
@@ -33,13 +35,34 @@ export class AuthPage implements OnInit {
   })
 
   firebaseSvc = inject(FirebaseService);
+  utilsSVC = inject(UtilsService)
 
   ngOnInit(){
 
 
   }
 
-  submit(){
-    console.log(this.form.value);
+  async submit(){
+    if(this.form.valid){
+
+      const loading = await this.utilsSVC.loading();
+      await loading.present();
+
+      this.firebaseSvc.signIn(this.form.value as User).then(res => {
+        console.log(res);
+      }).catch(error => {
+        console.log(error);
+
+        this.utilsSVC.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+      }).finally(() => {
+        loading.dismiss();
+      })
+    }
   }
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/compat/auth';
-import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile} from 'firebase/auth';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -10,9 +10,19 @@ export class FirebaseService {
 
   auth = inject(AngularFireAuth);
 
-  //==Autenticacion
-
+  //========Autenticacion
+  //Acceder
   signIn(user: User){
     return signInWithEmailAndPassword(getAuth(),user.email, user.password);
+  }
+
+  //Registar  
+  signUp(user: User){
+    return createUserWithEmailAndPassword(getAuth(),user.email, user.password);
+  }
+
+  //Actualizar
+  updateUser(displayName: string){
+    return updateProfile(getAuth().currentUser, {displayName})
   }
 }
